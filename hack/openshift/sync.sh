d362d05f6fc6f2bbe89816c4c2bae580af156522
Kevin Hannon <kehannon@redhat.com>
2025-07-14 14:49:34 -0400

    <CARRY> create a script and onboard release 0.12

---
+++ hack/openshift/sync.sh
@@ -0,0 +1,21 @@
+# Make sure you have a openshift remote
+
+# get readme openshift template and update if there is anything missing in branch
+git checkout openshift/main README_OPENSHIFT.md
+
+# get Dockerfiles
+# commented out because we don't need to sync this.
+#git checkout openshift/release-0.11 Dockerfile.rhel
+
+# get openshift make files
+git checkout openshift/release-0.11 Makefile-test-ocp.mk
+git checkout openshift/release-0.11 Makefile.ocp
+git checkout openshift/release-0.11 hack/e2e-test-ocp.sh
+git checkout openshift/release-0.11 hack/deploy-cert-manager-ocp.sh
+
+# step get dep-crds folder
+git checkout openshift/release-0.11 dep-crds
+
+# step get ocp kustomize configs
+git checkout openshift/release-0.11 config/default-ocp
+
